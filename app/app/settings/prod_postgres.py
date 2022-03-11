76aaf09ca15104d2a32545718bfd8309cd275cb9
Pavel Mirosh <prapeller@mail.ru>
2022-03-11 20:06:09 +0300

    prod ingnore

--- app/app/settings/prod_postgres.py
+++ app/app/settings/prod_postgres.py
@@ -1,22 +1,14 @@
-from app.settings.base import *
+from app.settings.prod import *
 
-DEBUG = False
-
-ALLOWED_HOSTS = ['blackemployer.com']
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qvsz6f(6dm0+$y7r+btmd&k&%!ku&4b-8k#&fb3+1nxqr!mctk'
-
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'postgres',
         'NAME': 'blackemployer_db',
-        'PASSWORD': 'secretpass',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
-        'PORT': '5432'
+        'PORT': '54325'
     }
 }
-
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static/'
