a619ada203b3945f102c1396c546e2f7809f4499
Pavel Mirosh <prapeller@mail.ru>
2022-03-14 01:25:38 +0300

    deploy

--- scripts/linux/install_docker.sh
+++ scripts/linux/install_docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "======================================================"
-echo "| This script will install docker and docker-compose |"
+echo "======================================================"
+echo "installing docker"
+echo "======================================================"
 echo "======================================================"
 
 sudo apt-get remove docker docker-engine docker.io containerd runc
@@ -17,7 +19,24 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+echo "======================================================"
+echo "======================================================"
+echo "docker version:"
+docker --version
+echo "======================================================"
+echo "======================================================"
 
-echo "=============================================="
-echo "| Install docker and docker-compose finished |"
-echo "=============================================="
+echo "======================================================"
+echo "======================================================"
+echo "installing docker-compose"
+echo "======================================================"
+echo "======================================================"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "======================================================"
+echo "======================================================"
+echo "docker-compose version:"
+docker-compose --version
+echo "======================================================"
+echo "======================================================"
