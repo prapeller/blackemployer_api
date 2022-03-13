006348d7102ac93d2e22f5eb506094652798049c
Pavel Mirosh <prapeller@mail.ru>
2022-03-13 16:37:40 +0300

    deploy

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN apk add --update --no-cache --virtual .tmp-build-deps \
     gcc libc-dev linux-headers postgresql-dev musl-dev zlib zlib-dev
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
+RUN pip install gunicorn
 RUN apk del .tmp-build-deps
 
 WORKDIR /app
---
+++ scripts/linux/install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "======================================================"
+echo "| This script will install docker and docker-compose |"
+echo "======================================================"
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+echo "=============================================="
+echo "| Install docker and docker-compose finished |"
+echo "=============================================="
