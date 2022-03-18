c9e258c5df9374db95f9f92eaab3672c08bd31ce
Pavel Mirosh <prapeller@mail.ru>
2022-03-18 17:49:46 +0300

    deploy

--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -2,11 +2,11 @@ from app.settings.base import *
 import os
 from dotenv import load_dotenv
 
-DEBUG = False
+load_dotenv(BASE_DIR / 'app/settings/.env')
 
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 STATIC_ROOT = '/vol/web/static/'
@@ -14,7 +14,6 @@ MEDIA_ROOT = '/vol/web/static/media'
 
 CSRF_TRUSTED_ORIGINS = ["https://blackemployer.com", "https://www.blackemployer.com"]
 
-load_dotenv(BASE_DIR / 'app/settings/.env')
 DOMAIN_NAME = "https://blackemployer.com"
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
