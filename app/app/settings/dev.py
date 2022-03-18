cf93be2ba59465ff727f1e56d7782005932fdc18
Pavel Mirosh <prapeller@mail.ru>
2022-03-18 22:52:07 +0300

    deploy

--- app/app/settings/dev.py
+++ app/app/settings/dev.py
@@ -2,29 +2,28 @@ from app.settings.base import *
 import os
 from dotenv import load_dotenv
 
+load_dotenv(BASE_DIR / 'app/settings/.env')
+
 DEBUG = True
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'supertopsecretproductionkey'
-
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS += [
-    'django_extensions',
-]
+SECRET_KEY = 'dev_key'
 
-
-MEDIA_ROOT = BASE_DIR / 'static/media/'
-
-REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append(
-        'rest_framework.renderers.BrowsableAPIRenderer',
-)
-
-
-load_dotenv(BASE_DIR / 'app/settings/.env')
 DOMAIN_NAME = 'http://127.0.0.1:8000'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
+
+MEDIA_ROOT = BASE_DIR / 'static/media/'
+
+INSTALLED_APPS += [
+    'django_extensions',
+]
+
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'].append(
+        'rest_framework.renderers.BrowsableAPIRenderer',
+)
+
+
--- app/app/settings/prod.py
+++ app/app/settings/prod.py
@@ -9,14 +9,14 @@ ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-STATIC_ROOT = '/vol/web/static/'
-MEDIA_ROOT = '/vol/web/static/media'
-
-CSRF_TRUSTED_ORIGINS = ["https://blackemployer.com", "https://www.blackemployer.com"]
-
 DOMAIN_NAME = "https://blackemployer.com"
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
+
+STATIC_ROOT = '/vol/web/static/'
+MEDIA_ROOT = '/vol/web/static/media'
+
+CSRF_TRUSTED_ORIGINS = ["https://blackemployer.com", "https://www.blackemployer.com"]
