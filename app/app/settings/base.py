2098226b364b9a9ad906960aafc2f3b882573595
Pavel Mirosh <prapeller@mail.ru>
2022-03-18 17:34:33 +0300

    deploy

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -108,7 +108,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
